3d7f0ddb1bd5ca9322291dfd9730462584213eba
Aqil Zeeshan <azeeshan@freewheel.com>
2022-07-25 18:33:04 +0100

    Working API using Prisma

---
+++ test.js
@@ -0,0 +1,27 @@
+const {PrismaClient} = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const newUser = await prisma.user.create({
+      data: {
+        name: 'Alice',
+        email: 'alice@prisma.io',
+        posts: {
+          create: {
+            title: 'Hello World',
+          },
+        },
+      },
+    })
+    console.log('Created new user: ', newUser)
+  
+    const allUsers = await prisma.user.findMany({
+      include: { posts: true },
+    })
+    console.log('All users: ')
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch((e) => console.error(e))
